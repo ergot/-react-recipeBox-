002cab05c0f1df889b1f042b51f2a4a2ed675ea6
ergot <wjose14@hotmail.com>
2017-02-08 15:20:16 +0100

    4.27

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import Header from './Header';
+import Admin from './Admin';
+import recettes from '../recettes';
 
 class App extends React.Component {
+
+  state = {
+    recettes: {}
+  };
+
+  chargerExemple = () => {
+    this.setState({ recettes });
+  };
 
   render() {
     return (
       <div className="box">
-        <Header pseudo={this.props.params.pseudo}></Header>
+        <Header pseudo={this.props.params.pseudo}/>
         <div className="cards">
           <div className="card"></div>
         </div>
+        <Admin chargerExemple={this.chargerExemple}/>
       </div>
     )
   }
